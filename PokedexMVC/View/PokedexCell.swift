4564043e2b92a378d6866475cb2451f36b31577b
mitsuyoshi <m123matsuo@icloud.com>
2019-04-12 16:08:58 +0900

    Initial commit

---
+++ PokedexMVC/View/PokedexCell.swift
@@ -0,0 +1,66 @@
+//
+//  PokedexCell.swift
+//  PokedexMVC
+//
+//  Created by mitsuyoshi matsuo on 2019/04/12.
+//  Copyright © 2019 mitsuyoshi matsuo. All rights reserved.
+//
+
+import UIKit
+
+class  PokedexCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .groupTableViewBackground
+        iv.contentMode = .scaleAspectFit
+        return iv
+    }()
+    
+    lazy var nameConteinerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .mainPink()
+        
+        view.addSubview(nameLabel)
+        nameLabel.center(inView: view)
+        
+        return view
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.text = "Bulbasaur"
+        return label
+    }()
+    
+    
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureViewComponents()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helper Functions
+    
+    func configureViewComponents() {
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+        
+        addSubview(imageView)
+        imageView.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: self.frame.height - 32)
+        
+        addSubview(nameConteinerView)
+        nameConteinerView.anchor(top: nil, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 32)
+    }
+}
---
+++ PokedexMVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokedexMVC
+//
+//  Created by mitsuyoshi matsuo on 2019/04/12.
+//  Copyright © 2019 mitsuyoshi matsuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
